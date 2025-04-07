fb8f88280b698e6485172ca49d8cdecff12b691a
ilfey <ilfey.global@gmail.com>
2025-04-08 03:18:08 +0700

    Remove unused code

--- frontend/src/shared/api/types.ts
+++ frontend/src/shared/api/types.ts
@@ -1,19 +1,15 @@
 export type ResponseWithMessage<Extension = {}> = {
-  message: string;
+	message: string
 } & Extension
 
 export type ErrorResponse<Schema extends {} = {}> = ResponseWithMessage<{
-  errors?: {
-    [K in keyof Schema]?: Array<string>
-  },
+	errors?: {
+		[K in keyof Schema]?: Array<string>
+	}
 }>
 
 export enum EnumSpaceTariff {
-  Pro = "Pro",
-  Free = "Free",
-  Enterprise = "Enterprise",
-}
-
-export enum EnumTaskStatus {
-
+	Pro = "Pro",
+	Free = "Free",
+	Enterprise = "Enterprise",
 }
